96ca88872f266b916c3e3d90022d5a59f235c66a
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-03 12:43:24 -0500

    Add simulator skeleton.

---
+++ src/cs412/dinghyprop/simulator/Simulator.java
@@ -0,0 +1,9 @@
+package cs412.dinghyprop.simulator;
+
+/**
+ * Simulator skeleton.
+ */
+public class Simulator {
+    public void invoke(String function) {}
+    public int  reference(String variable) { return 0; }
+}
