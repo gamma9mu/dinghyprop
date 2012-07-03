be0f291146812575b540dc49999da5873ea7818c
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-03 13:23:40 -0500

    Add some empty methods to Simulator to support the interpreter.

--- src/cs412/dinghyprop/simulator/Simulator.java
+++ src/cs412/dinghyprop/simulator/Simulator.java
@@ -6,4 +6,7 @@ package cs412.dinghyprop.simulator;
 public class Simulator {
     public void invoke(String function) {}
     public int  reference(String variable) { return 0; }
+    public int getTravelMetric() { return 0; }
+    public int getGoalDistanceMetric() { return 0; }
+    public int getSuccessMetric() { return 0; }
 }
