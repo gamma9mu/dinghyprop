219135c2b042d9054596f04a2578b2eb11b425db
Kevin Reuter <fastaccord1@gmail.com>
2012-07-05 20:23:35 -0500

    Fixed a mistake in the extends statement

--- src/cs412/dinghyprop/simulator/Obstacle.java
+++ src/cs412/dinghyprop/simulator/Obstacle.java
@@ -2,7 +2,7 @@ package cs412.dinghyprop.simulator;
 
 import java.lang.Math;
 
-public class Obstacle extends point{
+public class Obstacle extends Point{
 	
 	public Obstacle(int x, int y) {
 		super(x, y);
