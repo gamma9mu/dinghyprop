93a2b25745482d3736cda25112787a9f43d01ebf
Kevin Reuter <fastaccord1@gmail.com>
2012-07-05 21:44:15 -0500

    Class to allow reading simulation from file

---
+++ src/cs412/dinghyprop/simulator/SimulatorFile.java
@@ -0,0 +1,12 @@
+package cs412.dinghyprop.simulator;
+
+import java.io.File;
+import java.util.Scanner;
+public class SimulatorFile{
+	File file;
+	Scanner scanner;
+	
+	public SimulatorFile(File filename) {
+		
+	}
+}
