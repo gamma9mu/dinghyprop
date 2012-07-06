4cd16aeae3d93a39a94054c45571f237b1f9488e
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-05 20:42:41 -0500

    Remove trailing brace from Program.toString

--- src/cs412/dinghyprop/genetics/Program.java
+++ src/cs412/dinghyprop/genetics/Program.java
@@ -14,6 +14,6 @@ public class Program {
 
     @Override
     public String toString() {
-        return '[' + fitness + "] " + program + '}';
+        return '[' + fitness + "] " + program;
     }
 }
