368ffa69461999ce3495584ec2c9af3f4285d22d
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-05 20:32:15 -0500

    Comment Selector's method.

--- src/cs412/dinghyprop/genetics/Selector.java
+++ src/cs412/dinghyprop/genetics/Selector.java
@@ -4,5 +4,10 @@ package cs412.dinghyprop.genetics;
  * Specifies the interface of program selection methods.
  */
 public interface Selector {
+    /**
+     * Select a program from a population.
+     * @param population    The population to select from
+     * @return  One program from the population
+     */
     Program select(Program[] population);
 }
