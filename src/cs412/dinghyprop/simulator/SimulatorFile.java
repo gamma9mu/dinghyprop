51698605d955e74bfb6e2cf8f82780a249fe5ced
Kevin Reuter <fastaccord1@gmail.com>
2012-07-05 22:26:24 -0500

    Added test for file input

--- src/cs412/dinghyprop/simulator/SimulatorFile.java
+++ src/cs412/dinghyprop/simulator/SimulatorFile.java
@@ -1,12 +1,68 @@
 package cs412.dinghyprop.simulator;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
+/** 
+* This class allows for simulations to be read
+* from a file. Required format:
+* SizeX SizeY
+* DinghyX DinghyY
+* numObstacles 
+* GoalX GoalY
+* Obstacle1X Obstacle1Y
+* Obstacle2X Obstacle2Y
+* (Other obstacles)
+*/
 public class SimulatorFile{
 	File file;
-	Scanner scanner;
+	Scanner in;
+	Simulator sim;
+	int[] size, dinghy, goal;
+	int numObstacles;
 	
-	public SimulatorFile(File filename) {
+	public SimulatorFile(String filename) {
+		file = new File(filename);
+		try{
+			in = new Scanner(file);}
+		catch(IOException e){
+			System.err.println("Error!");
+			e.printStackTrace();
+		}
+		setSize();
+		setDinghy();
+		numObstacles = in.nextInt();
 		
+		sim = new Simulator(size[0], size[1], numObstacles, dinghy[0], dinghy[1]);
+		
+		setGoal();
+		sim.setGoal(goal[0], goal[1]);
+		System.out.println("Goal: " + goal[0] + " " + goal[1]);
+		for(int i = 0; i < numObstacles; i++){
+			int obstX = in.nextInt();
+			int obstY = in.nextInt();
+			sim.addObstacle(i, obstX, obstY);
+			System.out.println("Obstacle " + i + ": " + obstX + " " + obstY);
+		}
+		
+	}
+	
+	private void setSize(){
+		size = new int[2];
+		size[0] = in.nextInt();
+		size[1] = in.nextInt();
+	}
+	
+	private void setDinghy(){
+		dinghy = new int[2];
+		dinghy[0] = in.nextInt();
+		dinghy[1] = in.nextInt();
+	}
+	
+	private void setGoal() {
+		goal = new int[2];
+		goal[0] = in.nextInt();
+		goal[1] = in.nextInt();
 	}
 }
---
+++ src/cs412/dinghyprop/simulator/SimulatorFileTest.java
@@ -0,0 +1,7 @@
+package cs412.dinghyprop.simulator;
+
+public class SimulatorFileTest{
+	public static void main(String args[]){
+		SimulatorFile test = new SimulatorFile("C:\\Users\\kevin\\Google Drive\\github\\dinghyprop\\dinghyprop\\src\\cs412\\dinghyprop\\simulator\\test.txt");
+	}
+}
