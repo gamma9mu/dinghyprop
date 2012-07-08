97a6f41bf7ce329aa07afb047c71f8b23100d537
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-07 19:38:32 -0500

    Cleanup Goal.

--- src/cs412/dinghyprop/simulator/Goal.java
+++ src/cs412/dinghyprop/simulator/Goal.java
@@ -7,8 +7,8 @@ public class Goal extends Point{
 	}
 	
 	public boolean success(Dinghy dinghy) {
-		int dinghyPos[] = dinghy.getPosition();
-		int goalPos[] = this.getPosition();
+		int[] dinghyPos = dinghy.getPosition();
+		int[] goalPos = this.getPosition();
 		boolean success = false;
 		
 		if (dinghyPos[0] == goalPos[0] && dinghyPos[1] == goalPos[1]){
