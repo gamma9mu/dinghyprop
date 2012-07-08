ba72fad637008c78273761db9c9f3c5f05be7611
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-07 19:41:25 -0500

    ([]) varName(<-)[].

--- src/cs412/dinghyprop/simulator/randomSimTest.java
+++ src/cs412/dinghyprop/simulator/randomSimTest.java
@@ -1,13 +1,11 @@
 package cs412.dinghyprop.simulator;
 
-import java.util.Random;
-
 public class randomSimTest{
 	private static final int SIZEX = 300;
 	private static final int SIZEY = 300;
 	private static final int MAX_OBSTACLE = 20;
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		
 		SimulatorRandom sim = new SimulatorRandom(SIZEX, SIZEY, MAX_OBSTACLE);
 		
