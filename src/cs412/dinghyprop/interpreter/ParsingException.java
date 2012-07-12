0832e98fc74185c2e7c564a18956b09d5068af16
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-12 15:32:30 -0500

    ParsingException: Add message,cause c-tor.

--- src/cs412/dinghyprop/interpreter/ParsingException.java
+++ src/cs412/dinghyprop/interpreter/ParsingException.java
@@ -6,7 +6,11 @@ package cs412.dinghyprop.interpreter;
 public final class ParsingException extends Exception {
     private static final long serialVersionUID = -5447089429268408948L;
 
-    public ParsingException(String s) {
-        super(s);
+    public ParsingException(String message) {
+        super(message);
+    }
+
+    public ParsingException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
