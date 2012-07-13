0e311182fed0d23109f9b90ba1a5843b09dc03c4
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-13 00:41:59 -0500

    Program: Add fromString().

--- src/cs412/dinghyprop/genetics/Program.java
+++ src/cs412/dinghyprop/genetics/Program.java
@@ -1,15 +1,43 @@
 package cs412.dinghyprop.genetics;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Collects the details of a generated program.
  */
 public class Program {
+    private static final Pattern PARSE_PROGRAM =
+            Pattern.compile("^\\[(\\d+)\\]\\s+(.*)$");
     public final String program;
     int fitness;
 
+    /**
+     * Creates a {@code Program} object.
+     * @param program    The program text.
+     */
     public Program(String program) {
         this.fitness = -1;
         this.program = program;
+    }
+
+    /**
+     * Read a program from a line in the format of {@code toString()}.
+     * @param input    The line
+     * @return  A {@code Program} configured from {@code input} or an empty
+     * program if parsing failed.
+     */
+    public static Program fromString(String input) {
+        Matcher match = PARSE_PROGRAM.matcher(input);
+        if (match.matches()) {
+            String programText = match.group(2);
+            Program program = new Program(programText);
+            try {
+                program.fitness = Integer.parseInt(match.group(2));
+            } catch (NumberFormatException ignored) {}
+            return program;
+        }
+        return new Program("");
     }
 
     /**
