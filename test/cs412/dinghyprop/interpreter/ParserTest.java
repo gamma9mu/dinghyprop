8f7dca1fbede2ae259d351b4e8b62a1b8b0d46f0
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-12 20:04:54 -0500

    TestNG: Fix errant ParserTest code.

--- test/cs412/dinghyprop/interpreter/ParserTest.java
+++ test/cs412/dinghyprop/interpreter/ParserTest.java
@@ -7,48 +7,56 @@ import org.testng.annotations.Test;
  * TestNG for Parser
  */
 public class ParserTest {
-    String[] exprs = {
+    private String[] exprs = {
             "(hey there (brother) 6)",
             "(hey there(brother) 6)",
             "(hey there \n(brother) 6)",
             "(hey there\n(brother)6)",
             "(hey there \r\n(brother)\n6)",
-           "(hey there\r\n(brother)\r\n6)"
+            "(hey there\r\n(brother)\r\n6)"
     };
-    String mathy = "(+ (-(* 1 2)(/ 1 2))(^ 2 3))";
-    String exprThrows = "(+ 1 (- 1 3)";
 
     @Test
-    public void testParse() throws Exception {
+    public void testParseSimple() throws Exception {
         Expression expression = new Expression("hey");
         expression.addOperand("there");
         expression.addOperand(new Expression("brother"));
-        expression.addOperand(6);
+        expression.addOperand(Value.newInt(6));
+
+        Assert.assertTrue(expression.equals(expression));
 
         for (String string : exprs) {
             Parser p = new Parser(string);
             Assert.assertTrue(expression.equals(p.parse()));
         }
+    }
 
+    @Test
+    public void testParseMathy() throws Exception {
         Expression add = new Expression("+");
         Expression subtract = new Expression("-");
         Expression multiply = new Expression("*");
-        multiply.addOperand(1);
-        multiply.addOperand(2);
+        multiply.addOperand(Value.newInt(1));
+        multiply.addOperand(Value.newInt(2));
         subtract.addOperand(multiply);
         Expression divide = new Expression("/");
-        divide.addOperand(1);
-        divide.addOperand(2);
+        divide.addOperand(Value.newInt(1));
+        divide.addOperand(Value.newInt(2));
         subtract.addOperand(divide);
         add.addOperand(subtract);
         Expression raise = new Expression("^");
-        raise.addOperand(2);
-        raise.addOperand(3);
+        raise.addOperand(Value.newInt(2));
+        raise.addOperand(Value.newInt(3));
         add.addOperand(raise);
 
+        String mathy = "(+ (-(* 1 2)(/ 1 2))(^ 2 3))";
         Parser p = new Parser(mathy);
         Assert.assertTrue(add.equals(p.parse()));
+    }
 
+    @Test
+    public void testParseThrows() throws Exception {
+        String exprThrows = "(+ 1 (- 1 3)";
         Parser p0 = new Parser(exprThrows);
         try {
             p0.parse();
