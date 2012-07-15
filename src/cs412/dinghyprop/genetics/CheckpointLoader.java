74a1778bec9468dcb65e35566b474864c6bd0a09
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-14 21:50:10 -0500

    CpLoader: Add data line handler.

--- src/cs412/dinghyprop/genetics/CheckpointLoader.java
+++ src/cs412/dinghyprop/genetics/CheckpointLoader.java
@@ -1,16 +1,29 @@
 package cs412.dinghyprop.genetics;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Loads a checkpoint for continuing a GP run.
  */
 public final class CheckpointLoader {
+    private static final Pattern WHITESPACE = Pattern.compile("\\s+");
+
     private static Map<String, Constructor> selectors =
             new HashMap<String, Constructor>(1);
+
+    private BufferedReader in;
+
+    private int popSize;
+    private String selector;
+    private double crossOver;
+    private double mutation;
+    private double reproduction;
 
     /**
      * Register a constructor for a class that implements the {@code Selector}
@@ -60,5 +73,27 @@ public final class CheckpointLoader {
         return (Selector) ctor.newInstance();
     }
 
-    // # <pop_size> <selector> <x-over_rate> <mutation_rate> <reproduction_rate>
+    /**
+     * Parse the "data line" from a checkpoint file.  The line should have thr
+     * form:
+     * <pre>
+     # &lt;pop_size&gt; &lt;selector&gt; &lt;x-over_rate&gt; &lt;mutation_rate&gt; &lt;reproduction_rate&gt;
+     * </pre>
+     * including the leading '#'.
+     * @throws IOException  if an exception is thrown while reading the input
+     */
+    private void readDataLine() throws IOException {
+        String input = in.readLine();
+        while (input.trim().compareTo("") == 0) {
+            input = in.readLine();
+        }
+
+        String[] fields = WHITESPACE.split(input.trim());
+        assert (fields[0].compareTo("#") != 0);
+        popSize = Integer.parseInt(fields[1]);
+        selector = fields[2];
+        crossOver = Double.parseDouble(fields[3]);
+        mutation = Double.parseDouble(fields[4]);
+        reproduction = Double.parseDouble(fields[5]);
+    }
 }
