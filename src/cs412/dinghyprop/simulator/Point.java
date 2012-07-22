5ec68e1986738047b40ca053f3a9e736b87ee2c3
fastaccord1 <fastaccord1@gmail.com>
2012-07-22 18:55:57 -0500

    Started working on commenting. This commit is so I can move to laptop.

--- src/cs412/dinghyprop/simulator/Point.java
+++ src/cs412/dinghyprop/simulator/Point.java
@@ -1,22 +1,48 @@
 package cs412.dinghyprop.simulator;
 
+/**
+*  This is the superclass for Goal, Obstacle,
+*  and Dinghy. This class stores position information
+*  and provides methods to retrieve and manipulate
+*  that information
+*/
 public class Point {
 	private int posX;
 	private int posY;
 	
+	/**
+	*  Constructor that sets the initial position
+	*  of the point.
+	*  @param posX Initial X value of point
+	*  @param posY Initial Y value of point
+	*/	
 	public Point(int posX, int posY) {
 		this.posX = posX;
 		this.posY = posY;
 	}
 	
+	/**
+	*  Method to retrieve the current position
+	*  @return An array containing the current position
+	*/
 	public int[] getPosition() {
         return new int[]{posX, posY};
 	}
 	
+	/**
+	*  This method allows simulation to change the
+	*  X value of the point
+	*  @param x The new value of the X position
+	*/
 	protected void setX(int x) {
 		posX = x;
 	}
 	
+	/**
+	*  This method allows simulation to change the
+	*  Y value of the point
+	*  @param y The new value of the Y position
+	*/
 	protected void setY(int y) {
 		posY = y;
 	}
