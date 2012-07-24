620bfe6fadf034d0a7dbd497f48b05aec6fce6e3
Kevin Reuter <fastaccord1@gmail.com>
2012-07-24 12:38:46 -0500

    Added class to draw winner on screen

---
+++ src/cs412/dinghyprop/DrawWinner.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import cs412.dinghyprop.simulator.Simulator;
+import cs412.dinghyprop.simulator.Obstacle;
+
+public class DrawWinner extends JPanel {
+	private int sizeX;
+	private int sizeY;
+	private Simulator currentWinner;
+	
+	public DrawWinner() {
+		
+	}
+
+	@override
+	protected void paintComponent(Graphics g) {
+	
+	} 
+	
+	@override
+	public Dimension getPreferredSize() {
+	
+	}
+}
