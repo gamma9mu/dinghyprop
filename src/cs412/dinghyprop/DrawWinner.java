e98167fef08474615871c89fbd8eacf0f22dba10
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-27 13:44:24 -0500

    DrawWinner: Call dinghy drawing from paintComponent.

--- src/cs412/dinghyprop/DrawWinner.java
+++ src/cs412/dinghyprop/DrawWinner.java
@@ -18,16 +18,16 @@ public class DrawWinner extends JPanel implements Observer{
 	private Simulator currentWinner = null;
     private int[] goal = null;
 	private Obstacle[] obstacles = null;
-	private Graphics2D graph = null;
-	private static IMaster master = null;
+    private static IMaster master = null;
 	private static JComboBox dropDown = null;
 	private static ISimulator[] sims = null;
 	private static DrawWinner draw = null;
+    private int[] position = {0, 0};
 	
 	
 	public DrawWinner() {
-		sizeX = 300;
-		sizeY = 300;
+		sizeX = 30;
+		sizeY = 30;
 	}
 	
 	public void setSimulation(ISimulator current, Program prog) {
@@ -39,6 +39,7 @@ public class DrawWinner extends JPanel implements Observer{
 		
 		goal = currentWinner.getGoal();
 		obstacles = currentWinner.getObstacles();
+
 		try{
             Interpreter interpreter = new Interpreter(currentWinner, prog.program);
 			interpreter.run(100);
@@ -51,35 +52,34 @@ public class DrawWinner extends JPanel implements Observer{
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		graph = (Graphics2D)g;
+        Graphics2D graph = (Graphics2D) g;
 		if(currentWinner != null) {
 			graph.setColor(Color.GREEN);
 			graph.fillOval(goal[0] * 10, goal[1] * 10, 10, 10);
 		
 			graph.setColor(Color.RED);
-			drawObstacles();
-		}
-			
-	
-	} 
+			drawObstacles(g);
+            moveDinghy(g, position[0], position[1]);
+        }
+
+	}
 	
 	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension(sizeX * 10, sizeY * 10);
 	
 	}
-	
+
 	@Override
 	public void update(Observable o, Object arg) {
-		
-		int[] position = currentWinner.getDinghy();
-		moveDinghy(position[0], position[1]);
+        position = currentWinner.getDinghy();
 		System.out.println("Moving to " + position[0]);
+        repaint();
 	}
 	
-	public void moveDinghy(int posX, int posY) {
+	public void moveDinghy(Graphics g, int posX, int posY) {
 		this.repaint();
-		graph.setColor(Color.BLUE);
+		g.setColor(Color.BLUE);
 		int tempX = posX * 10;
 		int tempY = posY * 10;
 		int[] xPositions = null;
@@ -114,70 +114,70 @@ public class DrawWinner extends JPanel implements Observer{
 		} catch(cs412.dinghyprop.simulator.VariableReferenceException e) {
 			System.out.println("We caught something");
 		}
-		graph.fillPolygon(xPositions, yPositions, 5);
+		g.fillPolygon(xPositions, yPositions, 5);
 	}
 	
-	private void drawObstacles() {
+	private void drawObstacles(Graphics g) {
 		int count = 0;
 		for(Obstacle obstacle : obstacles) {
 			int[] position = obstacle.getPosition();
-			graph.fillOval(position[0] * 2, position[1] * 2, 10, 10);
+			g.fillOval(position[0] * 2, position[1] * 2, 10, 10);
 			count++;
 		}
 		System.out.println(count);
 	}
-	
-	
+
+
+    private void startAnimation() {
+        int index = dropDown.getSelectedIndex();
+        Program program = null;
+        try {
+            program = master.getCurrentLeader();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        ISimulator sim = sims[index];
+        draw.setSimulation(sim, program);
+    }
+
 	private static void createGui() {
-		
+
 		try {
 			sims = master.getEvaluationSimulators();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
+
 		dropDown = new JComboBox();
 		for(int i = 0; i < sims.length; i++) {
 			dropDown.addItem("Simulator " + i);
 		}
-		
+
+        draw = new DrawWinner();
+
 		JButton button = new JButton("Get Current Winner");
 		button.addActionListener(
 			new ActionListener() {
 				@Override
                 public void actionPerformed(ActionEvent e) {
-					startAnimation();
+					draw.startAnimation();
 				}
 			}
 		);
-		
-		draw = new DrawWinner();
-		
+
 		JFrame frame = new JFrame("animation");
 		frame.add(dropDown, BorderLayout.NORTH);
 		frame.add(draw, BorderLayout.CENTER);
 		frame.add(button, BorderLayout.SOUTH);
-		
+
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationByPlatform(true);
 		frame.setVisible(true);
-		
+
 	}
-	
-	private static void startAnimation() {
-		int index = dropDown.getSelectedIndex();
-		Program program = null;
-		try {
-			program = master.getCurrentLeader();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		ISimulator sim = sims[index];
-		draw.setSimulation(sim, program);
-	}
-	
+
 	public static void main(String[] args) {
 		String masterName = "//";
 		if(args.length == 0)
