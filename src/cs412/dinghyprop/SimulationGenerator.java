aa44c25e7a48cdad1f2afea93871ffcc64abf0f0
Kevin Reuter <fastaccord1@gmail.com>
2012-07-29 18:09:10 -0500

    Sims: added new simulations

---
+++ src/cs412/dinghyprop/SimulationGenerator.java
@@ -0,0 +1,11 @@
+package cs412.dinghyprop;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kevin
+ * Date: 7/29/12
+ * Time: 4:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimulationGenerator {
+}
