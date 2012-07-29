9006cd120793b55acac69fb1bdeeb3cf35c8eab4
Brian <brian@MOLLY>
2012-07-29 14:00:41 -0500

    Client: JOptionPane error reporting.

--- Client/src/cs412/dinghyprop/Client.java
+++ Client/src/cs412/dinghyprop/Client.java
@@ -2,6 +2,7 @@ package cs412.dinghyprop;
 
 import javax.jnlp.BasicService;
 import javax.jnlp.ServiceManager;
+import javax.swing.*;
 import java.net.InetAddress;
 
 /**
@@ -13,26 +14,35 @@ public class Client {
      * Create a client and register it with the master
      * @param address    The IP address of the master
      */
-    public Client(String address) throws Exception {
-        ClientImpl client = new ClientImpl(address);
-        ClientStatusWindow sw = new ClientStatusWindow(client);
-        sw.run();
-        client.initialize();
+    public Client(String address) {
+        try {
+            ClientImpl client = new ClientImpl(address);
+            ClientStatusWindow sw = new ClientStatusWindow(client);
+            sw.run();
+            client.initialize();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Cause: " + e.getLocalizedMessage(),
+                    "Error Initializing", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     /**
      * Optionally read an IP address from the CLI.
      * @param args    An optional IP address
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String injws = System.getProperty("injws");
         String address = "127.0.0.1";
 
         if (injws != null && !injws.trim().isEmpty()) {
-            BasicService service =
-                    (BasicService) ServiceManager.lookup("javax.jnlp.BasicService");
-            String host = service.getCodeBase().getHost();
-            address = InetAddress.getByName(host).getHostAddress();
+            try {
+                BasicService service = (BasicService) ServiceManager.lookup("javax.jnlp.BasicService");
+                String host = service.getCodeBase().getHost();
+                address = InetAddress.getByName(host).getHostAddress();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Cause: " + e.getLocalizedMessage(),
+                        "Error Looking Up Server", JOptionPane.ERROR_MESSAGE);
+            }
         } else if (args.length == 1) {
             address = args[0];
         } else {
