99a811fe982dcb110c22a3d29fbb222b32dc8327
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 16:02:59 -0500

    UnknownFunctionException: Improve javadoc.

--- Remotes/src/cs412/dinghyprop/simulator/UnknownFunctionException.java
+++ Remotes/src/cs412/dinghyprop/simulator/UnknownFunctionException.java
@@ -7,11 +7,16 @@
 package cs412.dinghyprop.simulator;
 
 /**
- * Exception for errors invoking nonexistent functions in the simulator.
+ * Exception class for errors caused by invoking nonexistent functions in the
+ * simulator.
  */
 public class UnknownFunctionException extends ExecutionException {
     private static final long serialVersionUID = -2305982772820403687L;
 
+    /**
+     * @param functionName    The name of the function whose invocation was
+     *                        attempted
+     */
     public UnknownFunctionException(String functionName) {
         super("Unknown function called: \"" + functionName + '"');
     }
