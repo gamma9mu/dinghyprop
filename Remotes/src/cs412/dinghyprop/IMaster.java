b190508491efb52129dfdb36d2c43a7873a8b87b
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 15:57:39 -0500

    IMaster: Improve javadoc.

--- Remotes/src/cs412/dinghyprop/IMaster.java
+++ Remotes/src/cs412/dinghyprop/IMaster.java
@@ -15,27 +15,40 @@ import java.rmi.RemoteException;
 
 /**
  * RMI Interface for the server.
+ *
+ * This interface provides necessary functionality for the setup and use of
+ * clients.
  */
 public interface IMaster extends Remote, Serializable {
 
     /**
-     * Obtain a copy of the evaluation simulators.
+     * Obtain a copy of the simulators that should <b>ALL</b> be used in the
+     * evaluation of programs.
+     *
      * @return  An array of the simulators to use to evaluate programs
-     * @throws RemoteException
+     * @throws RemoteException  if an RMI error occurs
      */
     ISimulator[] getEvaluationSimulators() throws RemoteException;
 
     /**
-     * Register an evaluation client to receive programs to evaluate.
+     * Register a client with the server as willing to evaluate program
+     * fitness. <b>Important:</b> there is no requirement that a client
+     * explicitly unregister itself from the server.
+     *
      * @param client    The client instance
-     * @throws RemoteException
+     * @throws RemoteException if an RMI error occurs
      */
     void registerClient(IClient client) throws RemoteException;
 
     /**
-     * Obtain a copy of the current best-fit program.
+     * Obtain a copy of the previous generations program with the highest
+     * fitness.
+     * <p>
+     * This method is intended for use by clients who wish to monitor the
+     * progress of the generations.
+     *
      * @return  A copy of the current leader
-     * @throws RemoteException
+     * @throws RemoteException if an RMI error occurs
      */
     Program getCurrentLeader() throws RemoteException;
 }
