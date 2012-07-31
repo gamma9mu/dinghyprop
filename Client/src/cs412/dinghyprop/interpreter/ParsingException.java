c05c07d580028a710f4af3eb92b7bce99d60e122
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 17:00:31 -0500

    ParsingException: Improve javadoc.

--- Client/src/cs412/dinghyprop/interpreter/ParsingException.java
+++ Client/src/cs412/dinghyprop/interpreter/ParsingException.java
@@ -7,15 +7,22 @@
 package cs412.dinghyprop.interpreter;
 
 /**
- * For parsing errors.
+ * Exception class for errors that occur while parsing generated programs
  */
 public final class ParsingException extends Exception {
     private static final long serialVersionUID = -5447089429268408948L;
 
+    /**
+     * @param message    a description of the cause
+     */
     public ParsingException(String message) {
         super(message);
     }
 
+    /**
+     * @param message    a description of the cause (context)
+     * @param cause      the exception that cause parsing to fail
+     */
     public ParsingException(String message, Throwable cause) {
         super(message, cause);
     }
