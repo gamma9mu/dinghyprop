6574a8bdf7f4a474f96d8385232d89521c3eac1d
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 20:10:27 -0500

    Obstacle: Improve javadoc.

--- Simulator/src/cs412/dinghyprop/simulator/Obstacle.java
+++ Simulator/src/cs412/dinghyprop/simulator/Obstacle.java
@@ -7,17 +7,17 @@
 package cs412.dinghyprop.simulator;
 
 /**
-*  This class is a subclass of Point and allows for
-*  Obstacle objects to be created.
-*/
+ * This class is a subclass of Point and allows for Obstacle objects to be
+ *  created.
+ */
 public class Obstacle extends Point{
 
 	/**
-	*  The constructor that sets up the position
-	*  of the obstacle.
-	*  @param x The X position of the obstacle.
-	*  @param y The Y position of the obstacle.
-	*/
+	 * The constructor that sets up the position of the obstacle.
+     *
+	 * @param x The X position of the obstacle.
+	 * @param y The Y position of the obstacle.
+	 */
 	public Obstacle(int x, int y) {
 		super(x, y);
 	}
