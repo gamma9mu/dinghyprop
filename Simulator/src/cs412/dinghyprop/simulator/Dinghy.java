000c37ecd1acc6170dbffbc9528a9f8b912e8b1f
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 20:31:00 -0500

    Dinghy: Improve javadoc.

--- Simulator/src/cs412/dinghyprop/simulator/Dinghy.java
+++ Simulator/src/cs412/dinghyprop/simulator/Dinghy.java
@@ -7,13 +7,13 @@
 package cs412.dinghyprop.simulator;
 
 /**
-*  This class stores current information about the
-*  dinghy that is in the simulation
-*/
+ * This class stores current information about the dinghy in a simulation.
+ */
 public class Dinghy extends Point{
     private static final long serialVersionUID = -4129944364781066537L;
+
     /**
-     * Variable to store the total distance travelled
+     * Stores the total distance travelled
      */
 	private int distTravelled;
 
@@ -23,16 +23,16 @@ public class Dinghy extends Point{
     private enum Direction { NORTH, EAST, SOUTH, WEST }
 
     /**
-     * Variable to store current direction of dinghy
+     * Stores current direction of dinghy
      */
     private Direction direc;
 
-	/**
-	*  Constructor that sets up initial position and
-	*  direction of the dinghy.
-	*  @param startX The initial X position of the dinghy.
-	*  @param startY The initial Y position of the dinghy.
-	*/
+    /**
+     * Constructor that sets up initial position and direction of the dinghy.
+     *
+     * @param startX The initial X position of the dinghy.
+     * @param startY The initial Y position of the dinghy.
+     */
 	public Dinghy(int startX, int startY) {
 		super(startX, startY);
 		distTravelled = 0;
@@ -40,11 +40,11 @@ public class Dinghy extends Point{
 	}
 	
 	/**
-	*  This constructor allows for the dinghy to be cloned
-	*  to allow for the same simulation to be used by all
-	*  slave programs.
-	*  @param dinghy This is the dinghy that needs to be cloned
-	*/
+ 	 * This constructor allows for the dinghy to be cloned to allow for the
+     * same simulation to be used by all slave programs.
+     *
+	 * @param dinghy This is the dinghy that needs to be cloned
+	 */
 	public Dinghy(Dinghy dinghy) {
 		super(dinghy.getPosition()[0], dinghy.getPosition()[1]);
 		distTravelled = dinghy.distTravelled;
@@ -53,6 +53,7 @@ public class Dinghy extends Point{
 
     /**
      * Wrap the dinghy around the map.
+     *
      * @param x    The x dimension of the map
      * @param y    The y dimension of the map
      */
@@ -73,8 +74,7 @@ public class Dinghy extends Point{
 	}
 
 	/**
-	*  Moves the dinghy a given distance in the
-	*  current direction.
+	 * Moves the dinghy a given distance in the current direction.
      */
 	protected void move() {
 		distTravelled++;
@@ -96,24 +96,22 @@ public class Dinghy extends Point{
 	}
 	
 	/**
-	*  Turns the dinghy to the right.
-	*/
+	 * Turns the dinghy to the right.
+	 */
 	protected void turnRight() {
 		direc = rightDirection();
 	}
 	
 	/**
-	*  Turns the dinghy to the left.
-	*/
+	 * Turns the dinghy to the left.
+	 */
 	protected void turnLeft() {
 		direc = leftDirection();
 	}
 	
 	/**
-	*  Returns the current direction of the
-	*  dinghy.
-	*  @return Returns the direction of the dinghy as an int.
-	*/
+	 * @return the current direction of the dinghy in degrees
+	 */
 	protected int getDirection() {
 		int result = 0;
 			switch(direc) {
@@ -134,18 +132,15 @@ public class Dinghy extends Point{
 	}
 	
 	/**
-	*  Gets the current distance that the dinghy
-	*  has travelled.
-	*  @return Returns the current distance travelled
-	*/
+	 * @return the current total distance travelled by the dinghy
+	 */
 	protected int getDistTravelled() {
 		return distTravelled;
 	}
 
     /**
-	*  Gets the direction to the left of the dinghy
-	*  @return The direction to the left of the dinghy.
-	*/
+	 * @return the direction to the left of the dinghy
+	 */
 	private Direction leftDirection() {
 		switch (direc) {
 			case NORTH:
@@ -161,9 +156,8 @@ public class Dinghy extends Point{
 	}
 
     /**
-	*  Gets the direction to the right of the dinghy.
-	*  @return The direction to the right of the dinghy.
-	*/
+	 * @return the direction to the right of the dinghy
+	 */
 	private Direction rightDirection() {
 		switch (direc) {
 			case NORTH:
@@ -179,9 +173,8 @@ public class Dinghy extends Point{
 	}
 
 	/**
-	*  Gets the direction to the rear of the dinghy.
-	*  @return The direction to the rear of the dinghy.
-	*/
+	 * @return the direction to the rear of the dinghy
+	 */
 	private Direction rearDirection() {
 		switch (direc) {
 			case NORTH:
@@ -197,11 +190,12 @@ public class Dinghy extends Point{
 	}
 
 	/**
-	*  Calculates the distance to the given obstacle in a specific direction.
-	*  @param direction The direction used to calculate distance.
-	*  @param obst The obstacle to which distance will be calculated.
-	*  @return The distance to the obstacle in the given direction(-1 if obstacle not in direction).
-	*/
+	 * Calculates the distance to the given obstacle in a specific direction.
+     *
+	 * @param direction the direction used to calculate distance.
+	 * @param obst the obstacle to which distance will be calculated.
+	 * @return the distance to the obstacle in the given direction(-1 if obstacle not in direction).
+	 */
 	protected int getDistanceInDirection(Direction direction, Obstacle obst) {
 		int[] obstPos = obst.getPosition();
 		int[] dinghyPos = this.getPosition();
@@ -241,51 +235,46 @@ public class Dinghy extends Point{
 	}
 	
 	/**
-	*  Gets the distance to the given obstacle in front
-	*  of the dinghy.
-	*  @param obst The current obstacle in distance calculation.
-	*  @return The distance to the obstacle(-1 if not in front of dinghy).
-	*/
+	 * @param obst an obstacle
+	 * @return the distance to the obstacle or -1 if it is not in front of the
+     * dinghy
+	 */
     protected int getDistanceFront(Obstacle obst) {
         return getDistanceInDirection(direc, obst);
     }
 
-	/**
-	*  Gets the distance to the given obstacle in the direction
-	*  to the left of the dinghy.
-	*  @param obst The current obstacle in distance calculation.
-	*  @return The distance to the obstacle(-1 if obstacle not to the left of dinghy).
-	*/
+    /**
+     * @param obst an obstacle
+     * @return the distance to the obstacle or -1 if it is not in to the left
+     * of the dinghy
+     */
 	protected int getDistanceLeft(Obstacle obst) {
         return getDistanceInDirection(leftDirection(), obst);
 	}
-	
-	/**
-	*  Gets the distance to the given obstacle in the direction
-	*  to the right of the dinghy.
-	*  @param obst The current obstacle in distance calculation.
-	*  @return The distance to the obstacle(-1 if obstacle not to the right of dinghy).
-	*/
+
+    /**
+     * @param obst an obstacle
+     * @return the distance to the obstacle or -1 if it is not in to the right
+     * of the dinghy
+     */
 	protected int getDistanceRight(Obstacle obst) {
         return getDistanceInDirection(rightDirection(), obst);
 	}
-	
-	/**
-	*  Gets the distance to the given obstacle in the direction
-	*  to the rear of the dinghy.
-	*  @param obst The current obstacle in distance calculation.
-	*  @return The distance to the obstacle(-1 if obstacle not behind dinghy).
-	*/
+
+    /**
+     * @param obst an obstacle
+     * @return the distance to the obstacle or -1 if it is not in behind the
+     * dinghy
+     */
 	protected int getDistanceRear(Obstacle obst) {
         return getDistanceInDirection(rearDirection(), obst);
 	}
 	
 	/**
-	*  Calculates the distance to the obstacle at a 45
-	*  degree angle to the left of the dinghy.
-	*  @param obst The current obstacle in distance calculation.
-	*  @return The distance to the obstacle (-1 if not at 45 degree angle to the left).
-	*/
+	 * @param obst an obstacle
+	 * @return the distance to the obstacle or -1 if it is not at 45 degree
+     * angle to the left of the dinghy
+	 */
 	protected int getDistanceShortLeft(Obstacle obst) {
 		int[] obstaclePosition = obst.getPosition();
 		int[] dinghyPosition = this.getPosition();
@@ -302,12 +291,11 @@ public class Dinghy extends Point{
 		return -1;
 	}
 	
-	/**
-	*  Calculates the distance to the obstacle at a 45
-	*  degree angle to the right of the dinghy.
-	*  @param obst The current obstacle in distance calculation.
-	*  @return The distance to the obstacle(-1 if not at a 45 degree angle to the right).
-	*/
+    /**
+     * @param obst an obstacle
+     * @return the distance to the obstacle or -1 if it is not at 45 degree
+     * angle to the right of the dinghy
+     */
 	protected int getDistanceShortRight(Obstacle obst) {
 		int[] obstaclePosition = obst.getPosition();
 		int[] dinghyPosition = this.getPosition();
@@ -325,14 +313,15 @@ public class Dinghy extends Point{
 	}
 	
 	/**
-	*  Checks to make sure that the slope is correct and the obstacle is in
-	*  the correct plane for the slope.
-	*  @param obstaclePos An array that stores the current obstacle position.
-	*  @param dinghyPos An array that stores the current dinghy position.
-	*  @param expectedVal Stores the expected slope value.
-	*  @param oper Tells method whether to use greater-than or less-than symbol
-	*  @return Returns true if slope is correct and obstacle is in correct plane.
-	*/
+	 * Checks to make sure that the slope is correct and the obstacle is in the
+     * correct plane for the slope.
+     *
+	 * @param obstaclePos xy-array that stores the current obstacle position
+	 * @param dinghyPos xy-array that stores the current dinghy position
+	 * @param expectedVal the expected slope value
+	 * @param oper comparison specified as '&lt;' or '&gt;'
+	 * @return true if slope is correct and obstacle is in correct plane.
+	 */
 	private boolean checkConditions(int[] obstaclePos, int[] dinghyPos, int expectedVal, char oper) {
         if (oper == '<')
             return getSlope(obstaclePos, dinghyPos) == expectedVal && obstaclePos[1] < dinghyPos[1];
@@ -341,11 +330,12 @@ public class Dinghy extends Point{
 	}
 	
 	/**
-	*  Calculates the slope between the dinghy and the given obstacle.
-	*  @param obstaclePosition An array that stores the current obstacle position
-	*  @param dinghyPosition An array that stores the current dinghy position
-	*  @return The slope between the dinghy and the given obstacle.
-	*/
+	 * Calculates the slope between the dinghy and a given obstacle.
+     *
+	 * @param obstaclePosition xy-array that stores the current obstacle position
+	 * @param dinghyPosition xy-array that stores the current dinghy position
+	 * @return the slope between the dinghy and the obstacle
+	 */
 	private int getSlope(int[] obstaclePosition, int[] dinghyPosition) {
 		int result = 0;
 		int numerator = obstaclePosition[1] - dinghyPosition[1];
