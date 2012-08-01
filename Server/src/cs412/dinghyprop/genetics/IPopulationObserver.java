a2c33929d5cfd6a5fa38cdea3e12bd9ebf8f3e12
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 19:12:44 -0500

    IPopulationObserver: Improve javadoc.

--- Server/src/cs412/dinghyprop/genetics/IPopulationObserver.java
+++ Server/src/cs412/dinghyprop/genetics/IPopulationObserver.java
@@ -8,13 +8,15 @@ package cs412.dinghyprop.genetics;
 
 /**
  * Interface for objects which want to be notified of the creation of
- * individuals by a {@code GeneticProgram}.
+ * individuals by a {@link GeneticProgram}.
  */
 public interface IPopulationObserver {
+
     /**
      * Notifies the observer that a new individual was created.
-     * @param index         The GP's index of the individual
-     * @param individual    The individual itself
+     *
+     * @param index         the GeneticProgram's index of the individual
+     * @param individual    the individual itself
      */
     void individualCreated(int index, Program individual);
 }
