47337ae54b09c8c4a71b25611c87d6d28e77fbf1
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 20:12:26 -0500

    ISimulator: Improve javadoc.

--- Remotes/src/cs412/dinghyprop/simulator/ISimulator.java
+++ Remotes/src/cs412/dinghyprop/simulator/ISimulator.java
@@ -14,16 +14,18 @@ import java.io.Serializable;
 public interface ISimulator extends Cloneable, Serializable {
 
     /**
-     *  This method receives an action from the interpreter and
-     *  tells the dinghy to take that action.
+     *  This method receives an action from the interpreter and tells the
+     *  dinghy to take that action.
+     *
      *  @param function The action that must be taken by the dinghy
      *  @throws UnknownFunctionException if the action is not valid.
      */
     void invoke(String function) throws UnknownFunctionException;
 
     /**
-     *  This method receives a reference to a variable from the interpreter
-     *  and sends back its value.
+     *  This method receives a reference to a variable from the interpreter and
+     *  sends back its value.
+     *
      *  @param variable The variable that is referenced by the interpreter.
      *  @return The value of the referenced variable
      *  @throws VariableReferenceException If the variable does not exist
@@ -31,21 +33,17 @@ public interface ISimulator extends Cloneable, Serializable {
     int reference(String variable) throws VariableReferenceException;
 
     /**
-     *  Calculates the fitness of the program. This is calculated by adding
-     *  the goal distance metric, the success metric, and the travel metric.
-     *  @return The fitness of the program
+     *  @return the fitness of the program
      */
     int getFitness();
 
     /**
-     * Determine whether execution can continue.
      * @return Whether execution can continue
      */
     boolean canContinue();
 
     /**
-     * Determine the fitness representing success for this environment.
-     * @return The goal fitness
+     * @return the fitness representing success for this environment
      */
     public int getTerminationFitness();
 
