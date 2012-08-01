b92b2c5af029317016209858eb66c0c2bd1c9362
Brian Guthrie <baguthrie@sbcglobal.net>
2012-07-31 19:11:59 -0500

    Selector: Improve javadoc.

--- Server/src/cs412/dinghyprop/genetics/Selector.java
+++ Server/src/cs412/dinghyprop/genetics/Selector.java
@@ -10,10 +10,11 @@ package cs412.dinghyprop.genetics;
  * Specifies the interface of program selection methods.
  */
 public interface Selector {
+
     /**
-     * Select a program from a population.
-     * @param population    The population to select from
-     * @return  One program from the population
+     * Selects a program from a population.
+     * @param population    the population to select from
+     * @return  one program from the population
      */
     Program select(Program[] population);
 }
